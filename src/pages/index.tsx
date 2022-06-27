1ec97efbf0a140edb627afd126a23077dc0a42d5
gabriel-frattini <gabriele.g.frattini@gmail.com>
2022-06-27 14:59:50 +0200

    setting up frontend w tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-
-interface Props {
-
-}
+import Head from "next/head";
+import { CallToAction } from "@/components/CallToAction";
+import { Faqs } from "@/components/Faqs";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { PrimaryFeatures } from "@/components/PrimaryFeatures";
+import { SecondaryFeatures } from "@/components/SecondaryFeatures";
+import { Testimonials } from "@/components/Testimonials";
+interface Props {}
 
 const Home: React.FC<Props> = () => {
   return (
-  <div>hello</div>
-  )
+    <>
+      <Head>
+        <title>TaxPal - Accounting made simple for small businesses</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;

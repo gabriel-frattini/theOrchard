8852975bedb4f8f2473738751fb59b2e642da735
gabriel-frattini <gabriele.g.frattini@gmail.com>
2022-08-09 20:48:53 +0200

    add default images on every room

--- src/components/room/index.tsx
+++ src/components/room/index.tsx
@@ -27,6 +27,9 @@ let data = {
         {
           url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1656766250/theOrchard/deluxeroom_k34ji5.jpg",
         },
+        {
+          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1660070383/theOrchard/deluxe-room-balcony_bz4dy4.jpg",
+        },
       ],
     },
     {
@@ -37,7 +40,7 @@ let data = {
         "A room with a double bed, your own bathroom and a lovely view over the ocean.",
       roomImages: [
         {
-          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1656766250/theOrchard/deluxeroom_k34ji5.jpg",
+          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1660070376/theOrchard/king-room_wxyy98.jpg",
         },
       ],
     },
@@ -49,7 +52,10 @@ let data = {
         "A room with a double bed, your own bathroom and a lovely view over the ocean.",
       roomImages: [
         {
-          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1656766250/theOrchard/deluxeroom_k34ji5.jpg",
+          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1660070412/theOrchard/queen-room_csxzln.jpg",
+        },
+        {
+          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1660070408/theOrchard/queen-room-balcony_kf8xtq.jpg",
         },
       ],
     },
@@ -61,7 +67,10 @@ let data = {
         "A room with a double bed, your own bathroom and a lovely view over the ocean.",
       roomImages: [
         {
-          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1656766250/theOrchard/deluxeroom_k34ji5.jpg",
+          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1660070396/theOrchard/twin-room-bathroom_nnbhzo.jpg",
+        },
+        {
+          url: "https://res.cloudinary.com/daaqbxnj6/image/upload/v1660070402/theOrchard/twin-room_nqupg0.jpg",
         },
       ],
     },

231f1bf6a44032e8a6ca1909ef286daa5f82632d
gabriel-frattini <gabriele.g.frattini@gmail.com>
2022-08-10 21:16:23 +0200

    ssg in index

--- src/components/room/ShowRooms.tsx
+++ src/components/room/ShowRooms.tsx
@@ -9,7 +9,7 @@ import { useUtils } from "@/utils/trpc";
 
 interface ParentCompProps {
   childComp?: React.ReactNode;
-  data: roomProps[];
+  data: unknown;
 }
 
 interface imageProp {
@@ -28,7 +28,7 @@ interface roomProps {
 export const ShowRooms: React.FC<ParentCompProps> = (props) => {
   const utils = useUtils();
   useEffect(() => {
-    props.data.forEach((room) =>
+    (props.data as roomProps[]).forEach((room) =>
       utils.prefetchQuery([
         "room.get-room-by-slug",
         { roomSlug: room.roomSlug },
@@ -85,64 +85,68 @@ export const ShowRooms: React.FC<ParentCompProps> = (props) => {
             <>
               <div className="flex pb-4 lg:pr-12 2xl:p-0  sm:pb-0 lg:col-span-5">
                 <Tab.List className="relative z-10 flex space-x-2 whitespace-nowrap  sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:space-y-1 lg:space-x-0 lg:whitespace-normal">
-                  {props.data.map((room: roomProps, featureIndex: number) => (
-                    <div
-                      key={featureIndex}
-                      className={clsx(
-                        "group relative rounded-full lg:min-w-[20rem] py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6",
-                        {
-                          "bg-white lg:bg-white lg:ring-1 lg:ring-inset lg:ring-white/100 shadow-xl shadow-slate-900/10":
-                            selectedIndex === featureIndex,
-                          "hover:bg-white/10 lg:hover:bg-white/5":
-                            selectedIndex !== featureIndex,
-                        }
-                      )}
-                    >
-                      <h3>
-                        <Tab
-                          className={clsx(
-                            "font-display text-lg [&:not(:focus-visible)]:focus:outline-none",
-                            {
-                              "text-black lg:text-black":
-                                selectedIndex === featureIndex,
-                              "text-gray-700 hover:text-black lg:text-gray-700":
-                                selectedIndex !== featureIndex,
-                            }
-                          )}
-                        >
-                          <span className="absolute inset-0  rounded-full lg:rounded-r-none lg:rounded-l-xl" />
-                          {room.roomName}
-                        </Tab>
-                      </h3>
-                      <p
-                        className={clsx("mt-2 hidden text-sm lg:block", {
-                          "text-black": selectedIndex === featureIndex,
-                          "text-black group-hover:text-black":
-                            selectedIndex !== featureIndex,
-                        })}
+                  {(props.data as roomProps[]).map(
+                    (room: roomProps, featureIndex: number) => (
+                      <div
+                        key={featureIndex}
+                        className={clsx(
+                          "group relative rounded-full lg:min-w-[20rem] py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6",
+                          {
+                            "bg-white lg:bg-white lg:ring-1 lg:ring-inset lg:ring-white/100 shadow-xl shadow-slate-900/10":
+                              selectedIndex === featureIndex,
+                            "hover:bg-white/10 lg:hover:bg-white/5":
+                              selectedIndex !== featureIndex,
+                          }
+                        )}
                       >
-                        {room.roomDescription}
-                      </p>
-                    </div>
-                  ))}
+                        <h3>
+                          <Tab
+                            className={clsx(
+                              "font-display text-lg [&:not(:focus-visible)]:focus:outline-none",
+                              {
+                                "text-black lg:text-black":
+                                  selectedIndex === featureIndex,
+                                "text-gray-700 hover:text-black lg:text-gray-700":
+                                  selectedIndex !== featureIndex,
+                              }
+                            )}
+                          >
+                            <span className="absolute inset-0  rounded-full lg:rounded-r-none lg:rounded-l-xl" />
+                            {room.roomName}
+                          </Tab>
+                        </h3>
+                        <p
+                          className={clsx("mt-2 hidden text-sm lg:block", {
+                            "text-black": selectedIndex === featureIndex,
+                            "text-black group-hover:text-black":
+                              selectedIndex !== featureIndex,
+                          })}
+                        >
+                          {room.roomDescription}
+                        </p>
+                      </div>
+                    )
+                  )}
                 </Tab.List>
               </div>
               <Tab.Panels className="lg:col-span-7 ">
-                {props.data.map((room: roomProps, idx: number) => (
-                  <Tab.Panel key={idx} unmount={false}>
-                    <Link href={`rooms/${room.roomSlug}`}>
-                      <div className="relative mx-auto mt-10 aspect-[1000/667.5] max-w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-lg shadow-gray-900/20 sm:w-auto lg:mt-0 lg:max-w-[50.8125rem]  hover:cursor-pointer ease-in duration-300 hover:shadow-2xl hover:shadow-gray-900/30">
-                        <Image
-                          src={room.roomImages[0].url}
-                          alt=""
-                          layout="fill"
-                          objectFit="contain"
-                          priority
-                        />
-                      </div>
-                    </Link>
-                  </Tab.Panel>
-                ))}
+                {(props.data as roomProps[]).map(
+                  (room: roomProps, idx: number) => (
+                    <Tab.Panel key={idx} unmount={false}>
+                      <Link href={`rooms/${room.roomSlug}`}>
+                        <div className="relative mx-auto mt-10 aspect-[1000/667.5] max-w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-lg shadow-gray-900/20 sm:w-auto lg:mt-0 lg:max-w-[50.8125rem]  hover:cursor-pointer ease-in duration-300 hover:shadow-2xl hover:shadow-gray-900/30">
+                          <Image
+                            src={room.roomImages[0].url}
+                            alt=""
+                            layout="fill"
+                            objectFit="contain"
+                            priority
+                          />
+                        </div>
+                      </Link>
+                    </Tab.Panel>
+                  )
+                )}
               </Tab.Panels>
             </>
           )}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,38 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Testimonials } from "@/components/Testimonials";
-import { Rooms } from "@/components/room";
 
 interface Props {}
 
-const Home: React.FC<Props> = () => {
+import { ShowRooms } from "../components/room/ShowRooms";
+
+import { createSSGHelpers } from "@trpc/react/ssg";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { appRouter } from "@/backend/router";
+import { trpc } from "@/utils/trpc";
+import { createContext } from "@/backend/router/context";
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>
+) {
+  const ctx = await createContext();
+  const ssg = await createSSGHelpers({
+    router: appRouter,
+    ctx,
+  });
+
+  const data = await ssg.fetchQuery("room.get-rooms");
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+    revalidate: 1,
+  };
+}
+
+const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { data, isLoading } = trpc.useQuery(["room.get-rooms"]);
+
   return (
     <body className="min-w-[600px]">
       <Head>
@@ -22,7 +49,8 @@ const Home: React.FC<Props> = () => {
       </Head>
       <main>
         <Hero />
-        <Rooms />
+
+        {!isLoading && <ShowRooms data={data} />}
         <Testimonials />
       </main>
       <Footer />
